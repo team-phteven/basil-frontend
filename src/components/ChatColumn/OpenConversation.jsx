9eafa2fe5d6d554ff69312f2d9c2693de5314f66
smith-sj <mr.sjsmith@protonmail.com>
2022-11-13 15:07:51 +1100

    fixed screen position

--- src/components/ChatColumn/OpenConversation.jsx
+++ src/components/ChatColumn/OpenConversation.jsx
@@ -1,33 +1,40 @@
 import React, { useState, useEffect } from "react";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import MessageInput from "./MessageInput";
 import { useConversations } from "../../contexts/ConversationsProvider";
 import { useUser } from "../../contexts/UserProvider";
 import MessageList from "./MessageList";
-import { io } from "socket.io-client";
-import { useSocket } from "../../contexts/SocketProvider";
+import styled from "styled-components";
 
 const OpenConversation = () => {
     const { localUser } = useUser();
 
-        const {
-            selectedConversation
-        } = useConversations();
-
+    const { selectedConversation } = useConversations();
 
     return (
-        <>
-            <Row className="mb-4 mx-2 flex-grow-1">
-                <MessageList/>
-            </Row>
-            <Row>
-                <MessageInput
-                    selectedConversation={selectedConversation}
-                    localUser={localUser}
-                />
-            </Row>
-        </>
+        <Row>
+            <Col className="vh-100 d-flex flex-column p-0">
+                <StyledRow className="m-0 flex-grow-1">
+                    <MessageList />
+                </StyledRow>
+                <Row className="m-0 p-0">
+                    <MessageInput
+                        selectedConversation={selectedConversation}
+                        localUser={localUser}
+                    />
+                </Row>
+            </Col>
+        </Row>
     );
 };
+
+const StyledRow = styled(Row)`
+    flex-direction: column-reverse;
+    overflow-y: scroll;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+`;
 
 export default OpenConversation;
