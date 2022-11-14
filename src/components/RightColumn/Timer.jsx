7f05b37c12884024dd2739bd8fe636db57936f50
rikifujihara <rikifujihara@gmail.com>
2022-11-14 12:26:46 +1100

    switching branches

---
+++ src/components/RightColumn/Timer.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect, useRef } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const Timer = () => {
+    const [time, setTime] = useState(0);
+
+    useEffect(() => {
+        return () => clearInterval(id.current);
+    }, []);
+
+    let id = useRef();
+
+    function handelTime() {
+        id.current = setInterval(() => {
+            setTime((prev) => prev + 1);
+            console.log(time);
+        }, 1000);
+    }
+
+    return (
+        <Row className="m-0 p-0">
+            <Col className="vh-100 d-flex flex-column">
+                <h1>{time}</h1>
+                <button onClick={() => handelTime()}>Start</button>
+                <button onClick={() => clearInterval(id.current)}>Pause</button>
+                <button
+                    onClick={() => {
+                        clearInterval(id.current);
+                        setTime(0);
+                    }}
+                >
+                    Reset
+                </button>
+            </Col>
+        </Row>
+    );
+};
+
+export default Timer;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,12 +6,12 @@ import ConversationList from "../components/ConversationList";
 import OpenConversation from "../components/ChatColumn/OpenConversation";
 import { useState } from "react";
 import Contacts from "../components/Contacts";
+import Timer from "../components/RightColumn/Timer";
 import Settings from "../components/Settings";
-import styled from 'styled-components';
-import UserMenu from '../components/UserMenu'
+import styled from "styled-components";
+import UserMenu from "../components/UserMenu";
 
 const Profile = () => {
-
     const [menu, setMenu] = useState("Conversations");
 
     return (
@@ -21,45 +21,47 @@ const Profile = () => {
                 <ConversationsColumn
                     xs={2}
                     style={{ boxSizing: "border-box", overflow: "hidden" }}
-                    className="p-0 m-0 m-0 p-0 d-flex flex-column vh-100">
+                    className="p-0 m-0 m-0 p-0 d-flex flex-column vh-100"
+                >
                     <Conversations className="flex-grow-1 m-0 p-0">
                         {menu === "Conversations" && <ConversationList />}
                         {menu === "Settings" && <Settings />}
                         {menu === "Contacts" && <Contacts />}
                     </Conversations>
-                    <UserMenu setMenu={setMenu} menu={menu}/>
+                    <UserMenu setMenu={setMenu} menu={menu} />
                 </ConversationsColumn>
 
                 <ChatColumn xs={8} className="d-flex flex-column vh-100">
-                    <OpenConversation className="vh-100"/>
+                    <OpenConversation className="vh-100" />
                 </ChatColumn>
 
-                <ChatContacts xs={2} className="m-0 p-0 vh-100"></ChatContacts>
+                <ChatContacts xs={2} className="m-0 p-0 vh-100">
+                    <Timer />
+                </ChatContacts>
             </Row>
         </StyledContainer>
     );
 };
 
 const ConversationsColumn = styled(Col)`
-background: var(--midgrey);
-`
-const Conversations = styled(Row)`
-`
+    background: var(--midgrey);
+`;
+const Conversations = styled(Row)``;
 
 const ChatColumn = styled(Col)`
     background: var(--lightgrey);
     padding: 0;
-`
+`;
 
 const ChatContacts = styled(Col)`
     background: var(--midgrey);
-`
+`;
 
 const StyledContainer = styled(Container)`
     height: 100vh;
     width: 100%;
     box-sizing: border-box;
     overflow: hidden;
-`
+`;
 
 export default Profile;
