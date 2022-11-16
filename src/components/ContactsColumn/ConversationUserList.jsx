cb071e93645008bddac609cca67053d75c6d17a2
rikifujihara <rikifujihara@gmail.com>
2022-11-16 12:29:17 +1100

    moved activeseconds state from provider to message input

--- src/components/ContactsColumn/ConversationUserList.jsx
+++ src/components/ContactsColumn/ConversationUserList.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useConversations } from "../../contexts/ConversationsProvider";
-import Row from "react-bootstrap/Row"
+import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 import { ContactSlab } from "../GlobalComponents/ContactSlab";
 
 export const ConversationUserList = () => {
@@ -9,15 +10,23 @@ export const ConversationUserList = () => {
     const [conversationUsers, setConversationUsers] = useState(null);
 
     useEffect(() => {
-        if (selectedConversation) setConversationUsers(selectedConversation.users);
+        if (selectedConversation)
+            setConversationUsers(selectedConversation.users);
     }, [selectedConversation]);
 
     return (
         <Row className="w-100 h-100 m-0 p-0">
             <Col className="p-0 m-0">
-            {conversationUsers && conversationUsers.map((user, index) => (
-                <ContactSlab contact={user} key={index} size="40px" fontSize="14px"/>     
-             ))}
+                {conversationUsers &&
+                    conversationUsers.map((user, index) => (
+                        <ContactSlab
+                            contact={user}
+                            key={index}
+                            size="40px"
+                            fontSize="14px"
+                        />
+                    ))}
+                <Button>Invite Contacts</Button>
             </Col>
         </Row>
     );
