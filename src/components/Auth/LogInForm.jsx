832ab43a05298bd1be12cdad79d6722090dd406a
smith-sj <mr.sjsmith@protonmail.com>
2022-11-21 14:11:00 +1100

    refactored login form

--- src/components/Auth/LogInForm.jsx
+++ src/components/Auth/LogInForm.jsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import styled from "styled-components";
 // custom components
 import PasswordFloatingLabelToggle from "./PasswordFloatingLabelToggle";
 // BS components
@@ -13,28 +14,29 @@ import Row from "react-bootstrap/Row";
 function LogInForm() {
     // navigate instance (for page reload)
     const navigate = useNavigate();
-    // state for form errors
-    // const [formErrors, setFormErrors] = useState({
-    //     email: "",
-    //     password: "",
-    // });
+
     // welcome back state
     const [welcomeBack, setWelcomeBack] = useState({
         email: "",
         name: "",
     });
+
     // formFields state
     const [formFields, setFormFields] = useState({
         email: "",
         password: "",
     });
+
     // state for component loading
     const [loading, setLoading] = useState(false);
 
     // load welcome back data
     useEffect(() => {
+        // get from local storage and parse
         const welcome = JSON.parse(localStorage.getItem("welcomeBack"));
+        // set welcome data in state
         setWelcomeBack(welcome);
+        // fill email field with stored email
         if (welcome) {
             setFormFields({
                 email: welcome.email,
@@ -43,8 +45,6 @@ function LogInForm() {
         }
     }, []);
 
-    
-
     // deconstruct formFields
     const { email, password } = formFields;
 
@@ -52,7 +52,9 @@ function LogInForm() {
     const handleSubmit = async (e) => {
         // prevent page refresh
         e.preventDefault();
+        // start loading state
         setLoading(true);
+        // try catch block for axios request
         try {
             // create new user session
             const { data } = await axios
@@ -65,16 +67,17 @@ function LogInForm() {
                         error.response.data.error
                     );
                     console.log(error_code);
-                    // s://basilchat-back-staging.herokuapp.comfilterFormError(error_code);
                     setLoading(false);
                     return;
                 });
             // store new user session in local storage
             localStorage.setItem("storedUser", JSON.stringify(data));
+            // store new welcome back data in local storage
             localStorage.setItem(
                 "welcomeBack",
                 JSON.stringify({ email: email, name: data.name })
             );
+            // end loading state
             setLoading(false);
             // refresh page
             navigate(0);
@@ -83,51 +86,26 @@ function LogInForm() {
         }
     };
 
-    // const filterFormError = (errors) => {
-    //     errors.map((error) => {
-    //         switch (error) {
-    //             case "WEAK_PASSWORD":
-    //                 setFormErrors({
-    //                     ...formErrors,
-    //                     password:
-    //                         "Must be > 8 characters long and contain at least a number, symbol, lowercase & uppercase letter.",
-    //                 });
-    //                 break;
-    //             case "EMAIL_FORMAT":
-    //                 setFormErrors({
-    //                     ...formErrors,
-    //                     email: "Must be in email address format e.g. example@mail.com",
-    //                 });
-    //                 break;
-    //             case "WRONG_CREDENTIALS":
-    //                 setFormErrors({
-    //                     ...formErrors,
-    //                     form: "This combination of email and password does not exist."
-    //                 });
-    //                 break;
-    //         }
-    //     });
-    // };
-
-    // create controlled inputs for form
+    // controlled inputs for form
     const handleInput = (e) => {
         const keyString = e.target.id;
         // remove the 'up' and 'in' prefix from email and password id's
+        // (see below for why these prefixes are needed)
         const key = keyString.substring(keyString.indexOf("-") + 1);
         const value = e.target.value;
         setFormFields({ ...formFields, [key]: value });
     };
 
     return (
-        <Row className="p-3">
+        <FormContainer>
             {welcomeBack && (
-                <span className="fs-3 my-3 text-white text-center">
+                <WelcomeMessage>
                     Welcome back, {welcomeBack.name.split(" ")[0]}
-                </span>
+                </WelcomeMessage>
             )}
             <Col as={Form} onSubmit={handleSubmit}>
                 <Row>
-                    <Form.Group as={Col} className="p-0 mb-4">
+                    <FormGroup as={Col}>
                         <Form.FloatingLabel label="Email">
                             <Form.Control
                                 className="border-0"
@@ -139,32 +117,52 @@ function LogInForm() {
                                 required
                             />
                         </Form.FloatingLabel>
-                    </Form.Group>
+                    </FormGroup>
                 </Row>
                 <Row>
-                    <Form.Group as={Col} className="p-0 mb-4">
+                    <FormGroup as={Col}>
                         <PasswordFloatingLabelToggle
+                            // because sign-up and sign-in are both loaded in tabs
+                            // we need to add unique id's for fields
                             uniqueId={"in-password"}
                             handleChange={handleInput}
                             value={formFields.password}
                         />
-                    </Form.Group>
+                    </FormGroup>
                 </Row>
                 <Row>
-                    <Col md={6} className="p-0">
-                        <Button
-                            variant="primary"
-                            type="submit"
-                            className="w-100 text-white"
-                            disabled={loading ? true : false}
-                        >
-                            Submit
-                        </Button>
+                    <Col
+                        as={Button}
+                        xs={12}
+                        md={6}
+                        variant="primary"
+                        type="submit"
+                        disabled={loading ? true : false}
+                    >
+                        Submit
                     </Col>
                 </Row>
             </Col>
-        </Row>
+        </FormContainer>
     );
 }
 
 export default LogInForm;
+
+const FormContainer = styled(Row)`
+    padding: 0;
+    margin: 0;
+`;
+
+const FormGroup = styled(Form.Group)`
+    padding: 0;
+    margin: 0px 0px 20px 0px;
+`;
+
+const WelcomeMessage = styled.span`
+    font-size: var(--fs-display);
+    color: var(--lightgrey);
+    text-align: center;
+    margin: 0 0 20px 0;
+    padding: 0;
+`;
